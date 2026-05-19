5e6f3b0690d318187a1a8bb57d842fd179764859
aditi05-11 <aditibiramane08@gmail.com>
2026-05-19 20:35:28 +0530

    updated code for navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,17 +26,17 @@ const Navbar = () => {
     >
       <div className="w-full px-6 md:px-12 lg:px-20 flex justify-between items-center">
         {/* Logo + Brand */}
-        <div className="flex items-center space-x-3">
-          <img
-            src="/images/logo.png"
-            alt="logo"
-            className="w-10 h-10 object-contain contrast-125"
-          />
+        <Link href="/" className="flex items-center space-x-3 cursor-pointer">
+    <img
+      src="/images/logo.png"
+      alt="logo"
+      className="w-10 h-10 object-contain contrast-125"
+    />
 
-          <h1 className="text-sm md:text-base font-semibold tracking-widest text-black">
-            SG FRAGRANCE HOUSE
-          </h1>
-        </div>
+    <h1 className="text-sm md:text-base font-semibold tracking-widest text-black">
+      SG FRAGRANCE HOUSE
+    </h1>
+        </Link>
 
         {/* Navigation */}
         <nav className="hidden md:flex space-x-10">
